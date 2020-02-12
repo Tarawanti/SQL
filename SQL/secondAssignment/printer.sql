a057406ac92c5215c4d501e0519bbc1feac6c977
Tarawanti <tarawanti1@gmail.com>
2020-02-11 22:08:52 -0500

    Querry for Second Assignment

---
+++ SQL/secondAssignment/printer.sql
@@ -0,0 +1,12 @@
+
+INSERT INTO [dbo].[printer]
+(model, color, type,price)
+VALUES('3001','true','int-jet','99'),
+        ('3002','false','laser','239'),
+        ('3003','true','laser','899'),
+        ('3004','true','int-jet','120'),
+        ('3005','false','laser','120'),
+        ('3006','true','int-jet','100'),
+        ('3007','true','laser','200')
+
+        GO
