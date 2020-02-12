7d2cb8f84b84a3de23ee27ed7c92fcf62459a22c
Tarawanti <tarawanti1@gmail.com>
2020-02-12 18:17:53 -0500

    ThirdQuestion

---
+++ SQL/secondAssignment/thirdPart.sql
@@ -0,0 +1,6 @@
+SELECT model, speed, hd FROM PC WHERE price<1000;
+SELECT model, speed AS megahertz, hd AS gigabytes FROM PC WHERE price <1000;
+SELECT maker, model FROM Product WHERE type='printer';
+SELECT model, ram, screen FROM Laptop WHERE price>1500;
+SELECT * from PRINTER WHERE color='true';
+SELECT model, hd FROM PC WHERE speed=3.20 AND price<2000;
