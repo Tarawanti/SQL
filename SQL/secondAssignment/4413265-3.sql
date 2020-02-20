c53d6a23e6371565aab45ebb77e81d25f74a7b2c
Tarawanti <tarawanti1@gmail.com>
2020-02-19 21:24:39 -0500

    Second Assignment

---
+++ SQL/secondAssignment/4413265-3.sql
@@ -0,0 +1,16 @@
+{\rtf1\ansi\ansicpg1252\cocoartf1671\cocoasubrtf200
+{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 (a).	SELECT model, speed, hd FROM PC WHERE price<1000;\
+(b).	SELECT model, speed AS megahertz, hd AS gigabytes FROM PC WHERE price <1000;\
+(c).	SELECT maker, model FROM Product WHERE type=\'91printer\'92;\
+(d).	SELECT model, ram, screen FROM Laptop WHERE price>1500;\
+(e).	SELECT * from PRINTER WHERE color=\'91true\'92\
+(f).	SELECT model, hd FROM PC WHERE speed=3.20 AND price<2000;\
+\
+\
+}
